70cb6dc5fe19691f774d35e22fd5ae49abebe8f2
hutapeasalomo@gmail.com <hutapeasalomo@gmail.com>
2019-09-15 00:15:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bawapulang/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bawapulang
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                replaceFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_feed -> {
+                replaceFragment(FeedFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_account -> {
+                replaceFragment(AccountFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nav_view.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
